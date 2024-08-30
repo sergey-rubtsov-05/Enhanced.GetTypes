02eb93e8620f550c55c79500d6a4f672279db456
duskembayev <d.uskembayev@gmail.com>
2024-08-30 15:04:00 +0300

    prepare project for deployment

--- playground/SampleLib/GetTypes.cs
+++ playground/SampleLib/GetTypes.cs
@@ -3,7 +3,7 @@ using SampleLib.ByInterface;
 
 namespace SampleLib;
 
-partial class GetTypes
+public partial class GetTypes
 {
     [DerivedTypes(typeof(IMarker), IncludeInternal = true, IncludeAbstract = true, IncludeInterfaces = true)]
     private partial IEnumerable<Type> ByInterface();
--- src/Enhanced.GetTypes.Annotation/DerivedTypesAttribute.cs
+++ src/Enhanced.GetTypes.Annotation/DerivedTypesAttribute.cs
@@ -1,13 +1,31 @@
 namespace Enhanced.GetTypes.Annotation;
 
+/// <summary>
+///     Attribute to specify the base type to get derived types from.
+/// </summary>
+/// <param name="baseType">
+///     The base type to get derived types from.
+/// </param>
 [AttributeUsage(AttributeTargets.Method)]
 public class DerivedTypesAttribute(Type baseType) : Attribute
 {
+    /// <summary>
+    ///     The base type to get derived types from.
+    /// </summary>
     public Type BaseType { get; } = baseType;
 
+    /// <summary>
+    ///     Whether to include interfaces.
+    /// </summary>
     public bool IncludeInterfaces { get; set; }
 
+    /// <summary>
+    ///     Whether to include internal types.
+    /// </summary>
     public bool IncludeInternal { get; set; }
 
+    /// <summary>
+    ///     Whether to include abstract types.
+    /// </summary>
     public bool IncludeAbstract { get; set; }
 }
